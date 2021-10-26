e234c815b9fc2a1b5edd307f282c8cf312b44466
Marcel Mravec <marcel.mravec@gmail.com>
2021-10-26 22:09:54 +0200

    Initial Commit

---
+++ Sekvence/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Sekvence
+//
+//  Created by Marcel Mravec on 26.10.2021.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
